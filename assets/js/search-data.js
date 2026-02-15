a2b8a6197fc0e4fc4ceb8d3077559da7b26ec9aa
ramchalamkr <ramchalamkr@gmail.com>
2026-02-15 03:28:44 +0000

    Deploying to gh-pages from @ ramchalamkr/ramchalamkr.github.io@0aa08f8660eb56f958f33faf29ff85b099739472 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,6 +23,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/patents/";
           },
+        },{id: "nav-blog",
+          title: "blog",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/blog/";
+          },
         },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
